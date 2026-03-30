f9285201efcabfef72f66655bf5f402e628014bf
Josh Eyres <josh.m.eyres@gmail.com>
2026-03-31 00:54:34 +0100

    Added a "file explorer" to the full portfolio page to allow me to add as many projects as i want without having loads of pages

---
+++ projects.js
@@ -0,0 +1,37 @@
+const projects = {
+  "project-alpha": {
+    "title": "Project Alpha",
+    "description": "A deep-sea research simulation.",
+    "bg": "url('Images/alpha-bg.jpg')", // Match your CSS variable format
+    "icon": "Images/alpha-icon.png",
+    "tags": ["Unity", "C#", "HLSL"]
+  },
+  "project-beta": {
+    "title": "Project Beta",
+    "description": "A retro-style OS interface.",
+    "bg": "url('Images/beta-bg.jpg')",
+    "icon": "Images/beta-icon.png",
+    "tags": ["HTML", "CSS", "JS"]
+  },
+  "project-charlie": {
+    "title": "Project Charlie",
+    "description": "A retro-style OS interface.",
+    "bg": "url('Images/beta-bg.jpg')",
+    "icon": "Images/beta-icon.png",
+    "tags": ["HTML", "CSS", "JS"]
+  },
+    "project-delta": {
+    "title": "Project Delta",
+    "description": "A retro-style OS interface.",
+    "bg": "url('Images/beta-bg.jpg')",
+    "icon": "Images/beta-icon.png",
+    "tags": ["HTML", "CSS", "JS"]
+  },
+    "project-echo": {
+    "title": "Project Echo",
+    "description": "A retro-style OS interface.",
+    "bg": "url('Images/beta-bg.jpg')",
+    "icon": "Images/beta-icon.png",
+    "tags": ["HTML", "CSS", "JS"]
+  }
+};
