ad82c0a3ca0f7e611d479f430ac1a14907e75fe2
Josh Eyres <josh.m.eyres@gmail.com>
2026-04-02 01:09:01 +0100

    Added gallery setup to "archive" projects

--- projects.js
+++ projects.js
@@ -1,18 +1,15 @@
 const projects = {
   "project-alpha": {
-    "title": "Project Alpha",
-    "description": `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eget arcu purus. Nunc pulvinar lobortis mollis. Nullam vulputate massa ac orci pellentesque scelerisque. Maecenas porta augue ipsum, in tempus neque euismod vitae. Phasellus mattis magna sit amet leo consequat hendrerit.
-    
-    Proin vitae dolor risus. Duis eget consectetur metus. Cras et ligula tristique, posuere ex vel, pellentesque mauris. Proin quis fringilla quam. Proin tempor luctus posuere. Praesent ipsum ex, interdum nec fringilla et, egestas vel dui. In viverra mauris quis leo feugiat eleifend. Suspendisse ultrices odio neque, sed porttitor elit egestas vel. Nam aliquet pulvinar tortor, sit amet luctus nunc sodales vitae. Nullam lobortis ultrices enim, sed commodo lectus malesuada ut. Suspendisse lacinia velit nec rutrum rutrum. In lacinia arcu ut nunc rhoncus, id faucibus tellus euismod. Mauris molestie congue ipsum, nec consectetur nisi egestas ut. Nullam non dapibus justo, eget sodales justo. Nulla tempor tristique pretium. Quisque in orci quam.Proin porttitor commodo semper. Nunc vel diam in metus gravida rhoncus. Nulla sed metus eu est porttitor hendrerit ut fermentum nisl. 
-    
-    Duis fermentum nisl eget posuere convallis. Pellentesque sodales tempus fringilla. Donec gravida, sem at pulvinar malesuada, orci augue mattis ipsum, id porttitor magna leo ac ex. Quisque neque augue, iaculis ac risus at, rhoncus congue diam. Proin faucibus velit sit amet rhoncus cursus. Morbi varius tortor sit amet nibh ultricies sodales. Curabitur faucibus eros ipsum, in fringilla ex molestie sed. Pellentesque at egestas lorem. Praesent et mauris rutrum, aliquam justo vitae, fermentum ante. Praesent consectetur luctus ex eget vulputate. Donec feugiat, erat efficitur sagittis venenatis, neque massa fringilla ex, nec iaculis ipsum metus vel purus. Maecenas vulputate aliquet vulputate. Nam ultricies purus mollis ante hendrerit pretium. Sed porttitor, odio sit amet cursus congue, justo dui blandit leo, a sodales quam lorem eget nisl. Mauris pulvinar sed diam sodales tempus. Maecenas sagittis iaculis dapibus. Donec tempus aliquam diam, vitae sollicitudin augue. Etiam suscipit rutrum erat, ut consectetur tortor congue vel. Etiam vestibulum faucibus tortor id feugiat. Nulla erat dui, luctus ut risus sed, commodo fringilla lorem. Aliquam imperdiet odio ut bibendum commodo. Nam ullamcorper lorem laoreet orci ornare, at cursus tellus fringilla.
-    
-    Vestibulum euismod lorem et luctus ullamcorper. Ut pulvinar sagittis lectus, in vehicula purus luctus a. Morbi malesuada massa sit amet sem tristique sollicitudin. Proin sem lorem, pharetra id pulvinar ut, pretium sed risus. Praesent sit amet lacinia orci. Curabitur blandit metus vitae nunc dignissim ullamcorper a non sem. Interdum et malesuada fames ac ante ipsum primis in faucibus. Aenean nunc augue, molestie eget sapien porta, scelerisque sollicitudin nisl.`,
+    "dirTitle": "Undergraduate Dissertation",
+    "title": "Undergraduate Dissertation - AI Prototyping",
+    "description": ``,
     "imgPath": "Images/LNDScreenshot.png", // Match your CSS variable format
+    "gallery": ["Images/LNDScreenshot.png", "Images/WhatGoesAroundComesAround.png", "Images/LNDScreenshot.png","Images/LNDScreenshot.png","Images/LNDScreenshot.png","Images/LNDScreenshot.png","Images/LNDScreenshot.png",],
     "icon": "Images/alpha-icon.png",
     "tags": ["Unity", "C#", "HLSL"]
   },
   "project-beta": {
+    "dirTitle": "Project Beta",
     "title": "Project Beta",
     "description": `A retro-style OS interface.`,
     "bg": "url('Images/beta-bg.jpg')",
@@ -20,6 +17,7 @@ const projects = {
     "tags": ["HTML", "CSS", "JS"]
   },
   "project-charlie": {
+    "dirTitle": "Project Charlie",
     "title": "Project Charlie",
     "description": `A retro-style OS interface.`,
     "bg": "url('Images/beta-bg.jpg')",
@@ -27,6 +25,7 @@ const projects = {
     "tags": ["HTML", "CSS", "JS"]
   },
     "project-delta": {
+    "dirTitle": "Project Delta",
     "title": "Project Delta",
     "description": `A retro-style OS interface.`,
     "bg": "url('Images/beta-bg.jpg')",
@@ -34,6 +33,7 @@ const projects = {
     "tags": ["HTML", "CSS", "JS"]
   },
     "project-echo": {
+    "dirTitle": "Project Echo",
     "title": "Project Echo",
     "description": `A retro-style OS interface.`,
     "bg": "url('Images/beta-bg.jpg')",
