9659c831a47b703ec99a68d31299c1540f9819c5
Josh Eyres <josh.m.eyres@gmail.com>
2026-04-02 01:20:28 +0100

    Some basic setup of projects

--- projects.js
+++ projects.js
@@ -2,16 +2,16 @@ const projects = {
   "project-alpha": {
     "dirTitle": "Undergraduate Dissertation",
     "title": "Undergraduate Dissertation - AI Prototyping",
-    "description": ``,
+    "description": `A dissertation exploring the of ai to allow programmers to rapidly prototype games without needing to wait for artists to finish`,
     "imgPath": "Images/LNDScreenshot.png", // Match your CSS variable format
     "gallery": ["Images/LNDScreenshot.png", "Images/WhatGoesAroundComesAround.png", "Images/LNDScreenshot.png","Images/LNDScreenshot.png","Images/LNDScreenshot.png","Images/LNDScreenshot.png","Images/LNDScreenshot.png",],
     "icon": "Images/alpha-icon.png",
     "tags": ["Unity", "C#", "HLSL"]
   },
   "project-beta": {
-    "dirTitle": "Project Beta",
-    "title": "Project Beta",
-    "description": `A retro-style OS interface.`,
+    "dirTitle": "Emmissive Raytracer",
+    "title": "Emmisive Raytracer Project",
+    "description": `Assignment for graphics and computational programming, a project about creating a raytracer that has emmissive lighting capabilities`,
     "bg": "url('Images/beta-bg.jpg')",
     "icon": "Images/beta-icon.png",
     "tags": ["HTML", "CSS", "JS"]
